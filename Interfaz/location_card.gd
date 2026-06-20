255f9555caf4b556545b97fbb7cf5a72ea1d5533
Danonino2712 <108773711+Danonino2712@users.noreply.github.com>
2026-06-20 16:45:02 -0300

    base inventario
    unos detalles que queria en el cartel de locación pero no se visualiza aún el color de fondo, solo el texto.
    Inventario básico: inventario en si, función para agregar y verifica que esté.
    Ahora veo de agregarlo y testear en un objeto

--- Interfaz/location_card.gd
+++ Interfaz/location_card.gd
@@ -3,7 +3,7 @@ extends Control
 @onready var label = $Label 
 #Nota para Dani (yo) que aún no aprende a usar todo: @onready declarar una variable y asignarle un valor automáticamente justo después de que el nodo y todos sus hijos hayan sido cargados y configurados en la escena
 
-func show_card(texto: String):
+func mostrarCartel(texto: String):
 	label.text = texto
 
 	visible = true
--- Mapas/restauranteRecepcion/restauranteRecepcion.gd
+++ Mapas/restauranteRecepcion/restauranteRecepcion.gd
@@ -5,6 +5,6 @@ class_name RestauranteRecepcion extends Mapa
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if VariablesJugador.progresoActual == VariablesJugador.Progreso.CONVERSACION_ELENA:
-		await location_card.show_card("Restaurante Familia Rossi - New York")
+		await location_card.mostrarCartel("Restaurante Familia Rossi - New York")
 		
-		print("Dani, aca va el dialogo con Elena")
+		print("Ahora agrego a Elena y le pongo el dialogo, ya que ahora mismo esta en npc")
