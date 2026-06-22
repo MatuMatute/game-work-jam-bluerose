8a9c1945f6dea35fe824edc5b31141df1291c679
Danonino2712 <108773711+Danonino2712@users.noreply.github.com>
2026-06-22 15:48:50 -0300

    mas dialogos con pistas incluidas

---
+++ Mapas/comedorPrivado/retratoDialogo.gd
@@ -0,0 +1,19 @@
+class_name Retrato extends NPC
+
+#Entiendo que esto ya al estar con npc no deberia estar(??????? pero por si acaso xD
+func _input_event(_viewport: Viewport, event: InputEvent, _shape_idx: int) -> void:
+	if !event is InputEventMouseButton:
+		return
+	if !event.button_index == MOUSE_BUTTON_LEFT:
+		return
+	if !event.is_released():
+		return
+	
+	var PistaHombresCuadro : Pista = load("res://Pistas/HombresCuadro.tres")
+	
+	var dialogoCuadros : DialogueResource = load("res://Dialogos/retratosDialogo.dialogue")
+	DialogueManager.show_dialogue_balloon(dialogoCuadros)
+	await DialogueManager.dialogue_ended
+	VariablesJugador.AgregarPista(PistaHombresCuadro)
+	VariablesJugador.progresoActual = VariablesJugador.Progreso.LLEGADO_ESCENA_CRIMEN
+	
--- Mapas/despachoOficina/despachoOficina.gd
+++ Mapas/despachoOficina/despachoOficina.gd
@@ -5,11 +5,17 @@ class_name DespachoOficina extends Mapa
 @onready var location_card = $LocationCard 
 
 func _ready() -> void:
-	# Chequea el singleton a ver si el jugador esta en el prologo --> necesito definitivamente mas comentarios jaja
+	# Chequea el singleton a ver si el jugador esta en el prologo 
+	
+	var pistaQuienContrata : Pista = load("res://Pistas/QuienContrata.tres")
+	var pistaQuienVictima : Pista = load("res://Pistas/QuienVictima.tres")
+	
 	match VariablesJugador.progresoActual:
 		VariablesJugador.Progreso.PROLOGO:
 			await location_card.mostrarCartel("Año 1945 - New York")
 			var dialogoPrologo : DialogueResource = load("res://Dialogos/ChiaraMichaelPrologo.dialogue")
 			DialogueManager.show_dialogue_balloon(dialogoPrologo)
 			await DialogueManager.dialogue_ended
+			VariablesJugador.AgregarPista(pistaQuienContrata)
+			VariablesJugador.AgregarPista(pistaQuienVictima)
 			VariablesJugador.progresoActual = VariablesJugador.Progreso.CONVERSACION_ELENA
