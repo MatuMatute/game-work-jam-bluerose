c4e6b1fe3b84097ef36479d83cb206ca84cbf3c5
MatuMatute <matiasgonzalezsandoval2@gmail.com>
2026-06-23 11:47:53 -0300

    Base del sistema de crafting, faltan las recetas de crafting nomás

--- Inventario/inventario.gd
+++ Inventario/inventario.gd
@@ -5,9 +5,6 @@ var objetos: Array[Item] = []
 #agrega en el array
 func agregarObjeto(objetoId:Item):
 	objetos.append(objetoId)
-	print("Objeto agregado:", objetoId)
-	#muestro el inventario para llevar un control¿
-	print(objetos)
 
 #verifica que este
 func tieneObjeto(objetoId:Item):
